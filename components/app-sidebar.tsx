decf4513267a67c9128b5274891c529119f4f135
Soikat Rahman <hello.dev.sr@gmail.com>
2025-01-07 15:00:17 +0600

    sidebar removed

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,28 +1,28 @@
-'use client';
-import * as React from 'react';
-import {
-	Sidebar,
-	SidebarContent,
-	SidebarGroup,
-	SidebarGroupContent,
-	SidebarHeader,
-	SidebarRail
-} from '@/components/ui/sidebar';
+// 'use client';
+// import * as React from 'react';
+// import {
+// 	Sidebar,
+// 	SidebarContent,
+// 	SidebarGroup,
+// 	SidebarGroupContent,
+// 	SidebarHeader,
+// 	SidebarRail
+// } from '@/components/ui/sidebar';
 
-import FilterForm from './location/filter/FilterFormProvider';
+// import FilterForm from './location/filter/FilterFormProvider';
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	return (
-		<Sidebar className='top-[--header-height]' variant='inset' {...props}>
-			<SidebarHeader></SidebarHeader>
-			<SidebarContent>
-				<SidebarGroup>
-					<SidebarGroupContent>
-						<FilterForm />
-					</SidebarGroupContent>
-				</SidebarGroup>
-			</SidebarContent>
-			<SidebarRail />
-		</Sidebar>
-	);
-}
+// export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+// 	return (
+// 		<Sidebar className='top-[--header-height]' variant='inset' {...props}>
+// 			<SidebarHeader></SidebarHeader>
+// 			<SidebarContent>
+// 				<SidebarGroup>
+// 					<SidebarGroupContent>
+// 						<FilterForm />
+// 					</SidebarGroupContent>
+// 				</SidebarGroup>
+// 			</SidebarContent>
+// 			<SidebarRail />
+// 		</Sidebar>
+// 	);
+// }
